1305585cb93d3a8fc80601cda3973401aca9b9b6
blackmouse572 <ngocvlqt1995@gmail.com>
2021-12-07 11:40:14 +0700

    package traveled histories models

---
+++ covid19_app/lib/models/travel_models/travel_places.dart
@@ -0,0 +1,9 @@
+class TravelPlaces {
+  String national;
+  String city;
+  String district;
+  String address;
+  DateTime time;
+  TravelPlaces(
+      this.national, this.city, this.district, this.address, this.time);
+}
