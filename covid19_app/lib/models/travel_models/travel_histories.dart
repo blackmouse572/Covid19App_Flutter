54e946a409998993e71a2ed5f1d16af7a734cf03
blackmouse572 <ngocvlqt1995@gmail.com>
2021-12-07 18:09:18 +0700

    Remove travel_hitories models, add toJson for place models

--- covid19_app/lib/models/travel_models/travel_histories.dart
+++
@@ -1,7 +0,0 @@
-import 'package:covid19_app/models/travel_models/travel_places.dart';
-
-class TravelHistory {
-  DateTime time;
-  List<TravelPlaces> travelPlaces;
-  TravelHistory(this.time, this.travelPlaces);
-}
--- covid19_app/lib/models/travel_models/travel_places.dart
+++ covid19_app/lib/models/travel_models/travel_places.dart
@@ -2,8 +2,14 @@ class TravelPlaces {
   String national;
   String city;
   String district;
-  String address;
+  String street;
   DateTime time;
-  TravelPlaces(
-      this.national, this.city, this.district, this.address, this.time);
+  TravelPlaces(this.national, this.city, this.district, this.street, this.time);
+  Map<String, dynamic> toJson() => {
+        "national": national,
+        "city": city,
+        "distric": district,
+        "street": street,
+        "time": time.toIso8601String()
+      };
 }
