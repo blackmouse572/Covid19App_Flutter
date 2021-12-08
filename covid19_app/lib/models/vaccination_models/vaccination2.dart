ec53b0768e17e967d0dd7c6ba6c6d110c8c08cb6
phanbaofptu <95603972+phanbaofptu@users.noreply.github.com>
2021-12-08 07:47:43 +0700

    Update overflow

--- covid19_app/lib/models/vaccination_models/vaccination2.dart
+++ covid19_app/lib/models/vaccination_models/vaccination2.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+void main() {
+  runApp(MaterialApp(home: vaccination2()));
+}
 class vaccination2 extends StatefulWidget {
   const vaccination2({ Key? key }) : super(key: key);
 
@@ -27,87 +30,91 @@ class _vaccination2State extends State<vaccination2> {
            ),
           ),
       ),
-      body: SizedBox(
-                        width: 336,
-                        height: 585,
-                        child: Material(
-                            color: Color(0xffdc143c),
-                            borderRadius: BorderRadius.circular(21),
-                            child: Padding(
-                                padding: const EdgeInsets.only(left: 27, right: 29, top: 61, bottom: 33, ),
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children:[
-                                        Container(
-                                            width: 90,
-                                            height: 90,
-                                            child: Image.asset(
-                    'assets/images/warrning.png',
-                    height: 90, 
-                    width: 90,),
-                                        ),
-                                        SizedBox(height: 151.50),
-                                        Text(
-                                            "FOR 0 PER VACCINATION",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 24,
-                                                fontFamily: "Roboto",
-                                                fontWeight: FontWeight.w900,
-                                            ),
-                                        ),
-                                        SizedBox(height: 151.50),
-                                        Container(
-                                            width: 272,
-                                            height: 70,
-                                            child: Row(
-                                                mainAxisSize: MainAxisSize.min,
-                                                mainAxisAlignment: MainAxisAlignment.center,
-                                                crossAxisAlignment: CrossAxisAlignment.center,
-                                                children:[
-                                                    SizedBox(
-                                                        width: 272,
-                                                        height: 70,
-                                                        child: Material(
-                                                            color: Colors.white,
-                                                            elevation: 25, 
-                                                            borderRadius: BorderRadius.circular(20),
-                                                            child: Padding(
-                                                                padding: const EdgeInsets.only(left: 30, right: 28, top: 13, bottom: 14, ),
-                                                                child: Row(
-                                                                    mainAxisSize: MainAxisSize.min,
-                                                                    mainAxisAlignment: MainAxisAlignment.center,
-                                                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                                                    children:[
-                                                                        SizedBox(
-                                                                            width: 214,
-                                                                            height: 43,
-                                                                            child: Text(
-                                                                                "Vaccination Registration",
-                                                                                textAlign: TextAlign.center,
-                                                                                style: TextStyle(
-                                                                                    color: Colors.black,
-                                                                                    fontSize: 18,
-                                                                                    fontFamily: "Roboto",
-                                                                                    fontWeight: FontWeight.w700,
-                                                                                ),
-                                                                            ),
-                                                                        ),
-                                                                    ],
-                                                                ),
-                                                            ),
-                                                        ),
-                                                    ),
-                                                ],
-                                            ),
-                                        ),
-                                    ],
-                                ),
-                            ),
-                        ),
-                    ),
+      body: Center(
+        child: SingleChildScrollView(
+        child: SizedBox(
+                          width: 336,
+                          height: 585,
+                          child: Material(
+                              color: Color(0xffdc143c),
+                              borderRadius: BorderRadius.circular(21),
+                              child: Padding(
+                                  padding: const EdgeInsets.only(left: 27, right: 29, top: 61, bottom: 33, ),
+                                  child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children:[
+                                          Container(
+                                              width: 90,
+                                              height: 90,
+                                              child: Image.asset(
+                      'assets/images/warrning.png',
+                      height: 90, 
+                      width: 90,),
+                                          ),
+                                          SizedBox(height: 151.50),
+                                          Text(
+                                              "FOR 0 PER VACCINATION",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 24,
+                                                  fontFamily: "Roboto",
+                                                  fontWeight: FontWeight.w900,
+                                              ),
+                                          ),
+                                          SizedBox(height: 151.50),
+                                          Container(
+                                              width: 272,
+                                              height: 70,
+                                              child: Row(
+                                                  mainAxisSize: MainAxisSize.min,
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  children:[
+                                                      SizedBox(
+                                                          width: 272,
+                                                          height: 70,
+                                                          child: Material(
+                                                              color: Colors.white,
+                                                              elevation: 25, 
+                                                              borderRadius: BorderRadius.circular(20),
+                                                              child: Padding(
+                                                                  padding: const EdgeInsets.only(left: 30, right: 28, top: 13, bottom: 14, ),
+                                                                  child: Row(
+                                                                      mainAxisSize: MainAxisSize.min,
+                                                                      mainAxisAlignment: MainAxisAlignment.center,
+                                                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                                                      children:[
+                                                                          SizedBox(
+                                                                              width: 214,
+                                                                              height: 43,
+                                                                              child: Text(
+                                                                                  "Vaccination Registration",
+                                                                                  textAlign: TextAlign.center,
+                                                                                  style: TextStyle(
+                                                                                      color: Colors.black,
+                                                                                      fontSize: 18,
+                                                                                      fontFamily: "Roboto",
+                                                                                      fontWeight: FontWeight.w700,
+                                                                                  ),
+                                                                              ),
+                                                                          ),
+                                                                      ],
+                                                                  ),
+                                                              ),
+                                                          ),
+                                                      ),
+                                                  ],
+                                              ),
+                                          ),
+                                      ],
+                                  ),
+                              ),
+                          ),
+                      ),
+    ),
+      )
     );
   }
 }
